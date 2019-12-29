7ec6c94aff7ad4c088589f1206665dbce68162d8
Miłosz <miloszpanas@gmail.com>
2019-12-29 14:22:18 +0100

    dodaj pozostale modele

--- spotlin/admin.py
+++ spotlin/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Gosc
+from .models import Gosc, Branza, Spotkanie, Specjalizacja, TypSpotkania
 
 admin.site.register(Gosc)
+admin.site.register(Branza)
+admin.site.register(Spotkanie)
+admin.site.register(Specjalizacja)
+admin.site.register(TypSpotkania)
--- spotlin/models.py
+++ spotlin/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Gosc(models.Model):
 
@@ -10,13 +12,64 @@ class Gosc(models.Model):
     nazwisko = models.CharField(max_length=200)
     telefon_komorkowy = models.IntegerField()
     telefon_stacjonarny = models.IntegerField()
-    branza = models.CharField(max_length=200)
+    branza = models.ForeignKey("spotlin.Branza", on_delete=models.CASCADE)
     email = models.EmailField(max_length=200)
     nazwa_firmy = models.CharField(max_length=200)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nazwa_firmy
 
 
+class Branza(models.Model):
+
+    class Meta:
+        verbose_name = "Branża"
+        verbose_name_plural = "Branże"
+
+    nazwa_branzy = models.CharField(max_length=200)
+    aktywna = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nazwa_branzy
+
+
+class Specjalizacja(models.Model):
+
+    class Meta:
+        verbose_name = "Specjalizacja"
+        verbose_name_plural = "Specjalizacje"
+
+    nazwa = models.CharField(max_length=100)
+    branza = models.ForeignKey("spotlin.Branza", on_delete=models.CASCADE)
+    aktywna = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Spotkanie(models.Model):
+
+    class Meta:
+        verbose_name = "Spotkanie"
+        verbose_name_plural = "Spotkania"
+
+    data = models.DateTimeField(default=timezone.now)
+    obecnosc = models.BooleanField(default=False)
+    typ_spotkania = models.ForeignKey("spotlin.TypSpotkania", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.typ_spotkania
+
+class TypSpotkania(models.Model):
+
+    class Meta:
+        verbose_name = "Typ spotkania"
+        verbose_name_plural = "Typy spoktania"
+
+    typ_spotkania = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.typ_spotkania
 
 
